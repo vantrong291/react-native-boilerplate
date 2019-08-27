1dae2847d2b6b255923c88c9a65ca3ce0c14c889
vantrong291 <vantrong291@gmail.com>
2019-08-27 11:52:30 +0700

    add splash screen

--- App/Screens/Root/RootScreen.js
+++ App/Screens/Root/RootScreen.js
@@ -6,10 +6,12 @@ import styles from './RootScreenStyle'
 import { connect } from 'react-redux'
 import StartupActions from 'App/Stores/Startup/Actions'
 import { PropTypes } from 'prop-types'
+import SplashScreen from 'react-native-splash-screen'
 
 class RootScreen extends Component {
   componentDidMount() {
     // Run the startup saga when the application is starting
+    SplashScreen.hide()
     this.props.startup()
   }
 
--- android/app/src/main/java/com/wefx/MainActivity.java
+++ android/app/src/main/java/com/wefx/MainActivity.java
@@ -1,9 +1,12 @@
 package com.wefx;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+import org.devio.rn.splashscreen.SplashScreen; // here
+
 
 public class MainActivity extends ReactActivity {
 
@@ -24,5 +27,11 @@ public class MainActivity extends ReactActivity {
                 return new RNGestureHandlerEnabledRootView(MainActivity.this);
             }
         };
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
     }
 }
